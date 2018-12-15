9faffd5212652d76fe4495d3e99fab6828cea6b6
jephdanso <jephdanso5@gmail.com>
2018-12-15 06:23:16 +0000

    Input validation added

---
+++ js/input_validation.js
@@ -0,0 +1,8 @@
+// Disabble all non numeric characters
+function isNumber(evt) {
+    var iKeyCode = (evt.which) ? evt.which : evt.keyCode
+    if (iKeyCode != 46 && iKeyCode > 31 && (iKeyCode < 48 || iKeyCode > 57))
+    return false;
+    
+    return true;
+}
